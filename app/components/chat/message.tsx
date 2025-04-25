3f75bf4456eb97bec06f84d2f7635ae8412a63cf
Valentin <valentin.schreibmaier@gmail.com>
2025-04-25 13:15:02 +0200

    Added MCP support
    * added MCP creation modal and tool usage
    
    * added view to see active MCP connections and their tools
    
    * added MCP view
    
    * added MCPs
    
    * removed debug console logs
    
    * added error handling for MCP connections
    
    * now using prepareToolsForAgent for all tool prep and fallback for possible undefined value
    
    * added messages as tool parameter
    
    * moved createClient to separate file
    
    * 0.1.15-0
    
    * 0.1.16-0
    
    * publishing alpha
    
    * added MCP message viewer
    
    * tag line fix
    
    * version update

--- app/components/chat/message.tsx
+++ app/components/chat/message.tsx
@@ -7,6 +7,34 @@ import { openBase64Pdf } from "~/lib/utils";
 import { ChatContext } from "./chat.client";
 import MarkdownViewer from "./markdownViewer";
 
+const formatValue = (value: any) => {
+  switch (typeof value) {
+    case "string":
+      // try to parse value as json
+      try {
+        const parsedValue = JSON.parse(value);
+        return formatValue(parsedValue);
+      } catch (error) {
+        return value;
+      }
+    case "object":
+      return (
+        <div>
+          {Object.entries(value).map(([key, value]) => {
+            return (
+              <div key={key}>
+                <span>{key}: </span>
+                <span>{formatValue(value)}</span>
+              </div>
+            );
+          })}
+        </div>
+      );
+    default:
+      return String(value);
+  }
+};
+
 interface MessageProps {
   message: MessageType;
   toolNames: Record<string, string>;
@@ -76,8 +104,54 @@ const Message: React.FC<MessageProps> = React.memo(
                 toolComponents[part.toolInvocation.toolName];
               const isDefaultTool =
                 part.toolInvocation.toolName.startsWith("default__");
+              const isMCPTool = !part.toolInvocation.toolName.includes("__");
               const hideDefaultTool =
                 isDefaultTool && !chatSettings?.showDefaultToolsDebugMessages;
+              if (isMCPTool && chatSettings?.showDefaultToolsDebugMessages) {
+                return (
+                  <div
+                    key={part.toolInvocation.toolCallId}
+                    className="oak-chat__message-tool-invocations"
+                  >
+                    <span className="oak-chat__message-tool-invocations-marker">
+                      <Terminal />
+                      {part.toolInvocation.toolName}
+                    </span>
+                    <div className="oak-chat__message-tool-invocations-mcp-content">
+                      <div>
+                        {Object.entries(part.toolInvocation.args).map(
+                          ([key, value]: [string, any]) => {
+                            return (
+                              <div
+                                className="oak-chat__message-tool-invocations-mcp-content-item"
+                                key={key}
+                              >
+                                <span>{key}:</span>
+                                <span>{formatValue(value)}</span>
+                              </div>
+                            );
+                          },
+                        )}
+                      </div>
+                      <div>
+                        {part.toolInvocation.state === "result" &&
+                          part.toolInvocation.result.content.map(
+                            (result: any) => {
+                              if (result.type === "text") {
+                                return (
+                                  <div key={result.id}>
+                                    <span>{formatValue(result.text)}</span>
+                                  </div>
+                                );
+                              }
+                              return null;
+                            },
+                          )}
+                      </div>
+                    </div>
+                  </div>
+                );
+              }
               if (!ToolComponent || hideDefaultTool) return null;
               return (
                 <div
@@ -85,7 +159,7 @@ const Message: React.FC<MessageProps> = React.memo(
                   className="oak-chat__message-tool-invocations"
                 >
                   <span className="oak-chat__message-tool-invocations-marker">
-                    <Terminal className="w-4 h-4" />
+                    <Terminal />
                     {toolNames[part.toolInvocation.toolName]}
                   </span>
                   <ToolComponent {...part.toolInvocation} />
--- app/lib/llm/generate.server.ts
+++ app/lib/llm/generate.server.ts
@@ -1,11 +1,8 @@
-import { generateText, type CoreMessage } from "ai";
-// import { getConfig } from "../config/config";
+import { generateText, type CoreMessage, type Message } from "ai";
 import { getSystemPrompt } from "./systemPrompts.server";
-import { getToolsForAgent } from "../tools/tools.server";
 import type { OAKConfig } from "~/types/config";
 import { getModelForAgent } from "./modelManager.server";
-import { getConfig } from "../config/config";
-import OAKProvider from "../lib";
+import { prepareToolsForAgent } from "./tools.server";
 
 export const generateSingleMessage =
   (config: OAKConfig) =>
@@ -17,28 +14,12 @@ export const generateSingleMessage =
       disableTools?: boolean;
     },
   ) => {
-    const system =
-      systemPrompt || (await getSystemPrompt("default", agentId)) || "";
-    const model = await getModelForAgent(agentId, config);
-    const tools = options?.disableTools
-      ? undefined
-      : await getToolsForAgent(agentId).then(async (r) => {
-          // get tools ready
-          return Promise.all(
-            r.map(async (t) => {
-              return [
-                t.identifier,
-                await t.tool({
-                  conversationId: "0",
-                  agentId,
-                  config: getConfig(),
-                  meta: {},
-                  provider: OAKProvider(getConfig(), t.pluginName as string),
-                }),
-              ];
-            }),
-          );
-        });
+    const [system = "", model, tools] = await Promise.all([
+      systemPrompt || getSystemPrompt("default", agentId),
+      getModelForAgent(agentId, config),
+      prepareToolsForAgent(agentId, "0", {}, []),
+    ]);
+
     const messages: CoreMessage[] = [
       {
         role: "system",
@@ -53,35 +34,23 @@ export const generateSingleMessage =
       model,
       toolChoice: options?.disableTools ? "none" : "auto",
       messages,
-      tools: tools ? Object.fromEntries(tools) : undefined,
+      tools: tools?.tools ? Object.fromEntries(tools.tools) : undefined,
     });
+    await tools.closeMCPs();
     return completion.text;
   };
 
 export const generateConversation =
-  (config: OAKConfig) => async (agentId: string, messages: CoreMessage[]) => {
-    const model = await getModelForAgent(agentId, config);
-    const tools = await getToolsForAgent(agentId).then(async (r) => {
-      // get tools ready
-      return Promise.all(
-        r.map(async (t) => {
-          return [
-            t.identifier,
-            await t.tool({
-              conversationId: "0",
-              agentId,
-              config: getConfig(),
-              meta: {},
-              provider: OAKProvider(getConfig(), t.pluginName as string),
-            }),
-          ];
-        }),
-      );
-    });
+  (config: OAKConfig) => async (agentId: string, messages: Message[]) => {
+    const [model, tools] = await Promise.all([
+      getModelForAgent(agentId, config),
+      prepareToolsForAgent(agentId, "0", {}, messages),
+    ]);
     const completion = await generateText({
       model,
-      tools: Object.fromEntries(tools),
+      tools: Object.fromEntries(tools.tools),
       messages,
     });
+    await tools.closeMCPs();
     return completion.text;
   };
