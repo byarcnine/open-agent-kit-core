1699d8e2a8a76d503a2400e23bccfad96d2f749c
valschr <valentin.schreibmaier@gmail.com>
2025-06-06 15:41:41 +0200

    fixed pathname access bug for global plugin routes

--- utils/withOAKContext.ts
+++ utils/withOAKContext.ts
@@ -20,7 +20,7 @@ export type OAKLoaderOrActionArgs = Omit<LoaderOrActionArgs, "context"> & {
 export const withOAKContext = serverOnly$(
   <T extends (args: OAKLoaderOrActionArgs) => Promise<any>>(fn: T) => {
     return (async (args: LoaderOrActionArgs) => {
-      const path = args.request.url.split("/");
+      const path = new URL(args.request.url).pathname.split("/");
       const indexOfPlugin = path.indexOf("plugins");
       // UI routes are prefix with /plugins/[pluginSlug|
       // global routes are prefix with the plugin identifier only
