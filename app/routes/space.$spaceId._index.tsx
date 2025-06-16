30c207c35a4a623af615d0983a1f35f95ac93337
valschr <valentin.schreibmaier@gmail.com>
2025-06-16 15:54:19 +0200

    cleaned console log

--- app/routes/space.$spaceId._index.tsx
+++ app/routes/space.$spaceId._index.tsx
@@ -182,8 +182,6 @@ const Index = () => {
     }
   }, []);
 
-  console.log("Filtered Agents:", filteredAgents);
-
   return (
     <Layout
       navComponent={<SpaceDetailNav space={space} userScopes={userScopes} />}
